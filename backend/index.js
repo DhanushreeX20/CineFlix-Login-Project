09b38198cf65dd03e84091feb4c5d32caefc62fd
unknown <dhanushree1120@gmail.com>
2025-12-07 23:19:06 +0530

    cineflix updated

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const USER = {
+  email: "check@here.com",
+  password: "1010",
+};
+
+app.get("/login", function (req, res) {
+  const { email, password } = req.query;
+  if (email === USER.email && password === USER.password) {
+     console.log("Login Successful ");
+    console.log("Email:", email);
+    console.log("Password:", password);
+      res.send( true,"Login successful" ) 
+  }
+  else{
+    console.log("Login failed , Wrong Data !");
+    console.log("Email:", email);
+    console.log("Password:", password);
+  res.send( false,"Invalid credentials" )
+
+  }
+});
+
+app.listen(5000, function () {
+  console.log("Server started...");
+});
